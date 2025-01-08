6d4310c6fc2035f3f7848a362ea3e3c09425b61a
yangkong2 <ayeongyang64@gmail.com>
2025-01-08 13:48:54 +0900

    Add files via upload

---
+++ 20250108.py
@@ -0,0 +1,80 @@
+# a = 4
+# b = 3
+
+# c = 1
+# d = 2
+# print (a+b)
+# print (c/d)
+
+# e = 7
+# f = 3
+# print (e//f)
+# print (e%f)
+
+
+# print(e**f)
+
+# quotient, remainder = divmod(5, 2)
+# print(quotient, remainder)
+
+# a = 11 + 2
+# b = 12 - 2
+# c = 13 * 2
+# d = 14 / 2
+# e = 15 ** 2
+# f = 16 // 2
+# g = 17 % 3
+# print( a, b, c, d, e, f, g)
+
+# i = 3
+# i += 3
+# print(i)
+# i -= 3
+# print(i)
+# i *=2
+# print(i)
+# i /=2 #나누기 값은 실수형
+# print(int (i))
+# i = 3
+# i %=2
+# print(i)
+
+# a = 10
+# b = 3
+# print("10 == 3 =>", a==b)
+# print("10 != 3 =>", a!=b)
+# print("10 > 3 =>", a>b)
+# print("10 >= 3 =>", a>=b)
+# print("10 < 3 =>", a<b)
+# print("10 <= 3 =>", a <=b)
+# print()
+
+# a = 13
+# print(bin (a))
+# print(hex(a))
+
+a = 12
+b = 8
+# print(bin(a))
+# print(bin(b))
+# print(bin(a&b))
+# print('')
+# print(a&b)
+
+# print('')
+# print(bin(a))
+# print(bin(b))
+# print(bin(a|b))
+# print('')
+# print(a|b)
+
+# print('')
+# print(bin(a))
+# print(bin(b))
+# print(bin(a^b))
+# print('')
+# print(a^b)
+
+print('')
+print(bin(a))
+print(bin(~a)) #1의 보수 2의 보수?
