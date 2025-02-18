e1622d752093a825dac1808790059a95dedee9ac
yangkong2 <ayeongyang64@gmail.com>
2025-02-18 22:58:52 +0900

    Add files via upload

---
+++ 20250218(1).py
@@ -0,0 +1,102 @@
+# text1 = "네이스123"
+# text2 = "123123"
+# text3 = "!@#$"
+# text4 = "!@#$123"
+# text5 = "wtf"
+# text6 = "wtf123123"
+# text7 = "한글1"
+# print(text1.isalnum())
+# print(text2.isalnum())
+# print(text3.isalnum())
+# print(text4.isalnum())
+# print(text5.isalnum())
+# print(text6.isalnum())
+# print(text7.isalnum())
+
+# text = "!@#!$!!214153스"
+# text1 = "네이스"
+# text2 = "123123"
+# text3 = "!@#$"
+# check = ""
+# print(text1.isalpha())
+# print(text2.isalpha())
+# print(text3.isalpha())
+# for i in text:
+#         if i.isalpha():
+#             check += i
+# print (check)
+
+# a = "BlockDMask" #문자로만 이루어짐
+# b = "1234Blog" #문자 + 숫자
+# c = "131231" #숫자
+# d = "-234" #숫자
+# e = "1.23" #소수점
+# f = "3²" #3의 2제곱 기호 숫자
+# g = "⅔" #수학 기호 숫자 2/3
+# h = "0" #0
+# i = "0123"
+# #0으로 시작한 숫자
+# #str.isdigit("문자열")
+# print(f"str.isdigit('{a})') : {str.isdigit(a)}")
+# print(f"str.isdigit('{b})') : {str.isdigit(b)}")
+# print(f"str.isdigit('{c})') : {str.isdigit(c)}")
+# print(f"str.isdigit('{d})') : {str.isdigit(d)}")
+# print(f"str.isdigit('{e})') : {str.isdigit(e)}")
+# print(f"str.isdigit('{f})') : {str.isdigit(f)}")
+# print(f"str.isdigit('{g})') : {str.isdigit(g)}")
+# print(f"str.isdigit('{i})') : {str.isdigit(i)}")
+# print()
+
+# #"문자열".isdigit()
+# print(f"'{a}'.isdigit() : {str.isdigit(a)}")
+# print(f"'{b}'.isdigit() : {str.isdigit(b)}")
+# print(f"'{c}'.isdigit() : {str.isdigit(c)}")
+# print(f"'{d}'.isdigit() : {str.isdigit(d)}")
+# print(f"'{e}'.isdigit() : {str.isdigit(e)}")
+# print(f"'{f}'.isdigit() : {str.isdigit(f)}")
+# print(f"'{g}'.isdigit() : {str.isdigit(g)}")
+# print(f"'{i}'.isdigit() : {str.isdigit(i)}")
+
+# a = "12345678"
+# print(a.isdigit())
+# print(a.isdecimal())
+# print(a.isnumeric())
+
+# x = '3²'
+# print(x)
+# print(x.isdigit())
+# #True
+# print(x.isdecimal())
+# #False
+# print(x.isnumeric())
+# int(x)
+# #ERROR
+
+# a = "I Love Python"
+
+# print(a.islower())
+# print(a.isupper())
+# print(a.upper())
+# print(a.lower())
+
+# str = "THIS is string example....wow!!!"
+# print(str.islower())
+
+# str = "this is string example....wow!!!"
+# print(str.islower())
+
+# str = "this is string example....wow!!!"
+# print(str.swapcase())
+# str = "THIS IS STRING EXAMPLE....WOW!!!"
+# print(str.swapcase())
+
+# str = "This Is String Example....Wow!!!"
+# print(str.istitle())
+# str = "This is string example....wow!!!"
+# print(str.istitle())
+
+# str = "This string example....wow!!!"
+# print (str.title())
+
+print('hello world!'.capitalize()) 
+print('HELLO WORLD!'.capitalize()) 
